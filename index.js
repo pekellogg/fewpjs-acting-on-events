d6c96ea01a97c2960e9534bb68149912dab7714d
Peyton Kellogg <pekellogg@hotmail.com>
2022-07-12 16:07:04 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Your code here
+// define element to be manipulated
+var dodger = document.getElementById("dodger");
+
+// define left movement
+function moveDodgerLeft() {
+    var leftNumbers = dodger.style.left.replace("px", "");
+    var left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) { dodger.style.left = `${left - 1}px`; }
+}
+
+// define right movement
+function moveDodgerRight() {
+    var leftNumbers = dodger.style.left.replace("px", "");
+    var left = parseInt(leftNumbers, 10);
+    if (left > 0) { dodger.style.left = `${left + 1}px`; }
+}
+
+// listen for left keypress event
+document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") { moveDodgerLeft(); }
+});
+
+// listen for right keypress event
+document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowRight") { moveDodgerRight(); }
+});
